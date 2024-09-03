fde8fc89f1d56253468a4ea9b96c607ab96ef20d
Nishantnishu01 <kumarnishu277@gmail.com>
2024-09-03 16:34:07 +0530

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,25 @@
+"use strict"; // treat all JS code as newer version
+// alert(3+3) // we are using nodejs, not browser
+
+console.log( 3+ 
+    3) // code readabilty should be high
+
+let name = "Nishnat" // string
+let age  = 25 // Number
+let isLoggedIn = false // boolean
+
+// primitive dataTypes
+//number => 2 to power 53
+// bigint
+// String => true/false
+// null => standalone value // blank 
+// undefined => //value nor defind
+// symbol => unique
+
+// object
+
+console.log(typeof null);// object
+console.log(typeof undefined);// undefined
+
+
+
