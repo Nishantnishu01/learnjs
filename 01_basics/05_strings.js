945618a657a8da28fe92f9760a851f4f3bf318f5
Nishantnishu01 <kumarnishu277@gmail.com>
2024-09-07 11:50:51 +0530

    strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,40 @@
+const name = "Nishant"
+const repoCount = 50 
+
+// conole.log(name + repoCount + " Value");
+
+console.log(`Hello my name is ${name} and  my  repo count is ${repoCount}`);
+
+const gameName = new String('Nishant')
+
+console.log(gameName[0]);
+console.log(gameName.__proto__);
+
+
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('h'));
+
+const newString = gameName.substring(0, 5)
+console.log(newString);
+
+const anotherString = gameName.slice(-5, 2)
+console.log(anotherString);
+ const  newStringOne = "   Nishu   " 
+ console.log(newStringOne);
+ console.log(newStringOne.trim());
+
+ const url = "https://Nishant.com/nishant%30home"
+ console.log(url.replace('%30', '_'))
+
+ console.log(url.includes('aman'));
+
+ console.log(gameName.split('_'));
+ 
+ 
+ 
+ 
+
+
+
